13c22677db0bad81f0d9ac632b839fb9e235f56f
Jesse Lumme <jesse@smilee.io>
2018-10-02 19:42:24 +0300

    Added Number.isNumber tests

---
+++ test/Number.js
@@ -0,0 +1,23 @@
+// Prepare chai
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Number', function() {
+  describe('static isNumber', function() {
+    it('string is not a number', function() {
+      expect(Number.isNumber('a')).to.be.false;
+    });
+
+    it('null is not a number', function() {
+      expect(Number.isNumber(null)).to.be.false;
+    });
+
+    it('undefined is not a number', function() {
+      expect(Number.isNumber('undefined')).to.be.false;
+    });
+
+    it('1 is a number', function() {
+      expect(Number.isNumber(1)).to.be.true;
+    });
+  });
+});
